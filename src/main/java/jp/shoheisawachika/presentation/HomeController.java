09db66fa0e58765d508cae418c5a1399a2aa1dff
Shohei Sawachika <sawasyo1989@gmail.com>
2024-10-15 20:42:40 +0900

    ・opencsvを導入

--- src/main/java/jp/shoheisawachika/presentation/HomeController.java
+++ src/main/java/jp/shoheisawachika/presentation/HomeController.java
@@ -11,6 +11,19 @@ public class HomeController extends CommonController {
 
     @GetMapping({"/", "/home"})
     public String home(HttpServletRequest request, Model model) {
+    	
+    	/*
+    	List<String[]> outpt = new ArrayList<String[]>();
+    	String path = "";
+    	File file = new File(path);
+    	try (CSVReader csvReader = new CSVReader(new FileReader(file))) {            
+    		outpt = csvReader.readAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //throw new Exception();
+        }
+        */
+    	
         return "home";
     }
 
